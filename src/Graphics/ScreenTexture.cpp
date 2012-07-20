ce8d03977224b65fb6e6404de0e77cf39cfdea6a
Noam Chitayat <noam.chitayat+git@gmail.com>
2012-07-20 17:43:51 -0400

    Adding copyright headers to new files

--- src/Graphics/ScreenTexture.cpp
+++ src/Graphics/ScreenTexture.cpp
@@ -1,3 +1,9 @@
+/*
+ *  This file is covered by the Ruby license. See LICENSE.txt for more details.
+ *
+ *  Copyright (C) 2007-2012 Noam Chitayat. All rights reserved.
+ */
+
 #include "ScreenTexture.h"
 #include "GraphicsUtil.h"
 #include "OpenGLExtensions.h"
--- src/Graphics/ScreenTexture.h
+++ src/Graphics/ScreenTexture.h
@@ -1,3 +1,9 @@
+/*
+ *  This file is covered by the Ruby license. See LICENSE.txt for more details.
+ *
+ *  Copyright (C) 2007-2012 Noam Chitayat. All rights reserved.
+ */
+
 #ifndef SCREEN_TEXTURE_H
 #define SCREEN_TEXTURE_H
 
