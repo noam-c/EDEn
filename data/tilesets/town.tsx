1c84144801c494da380b295f9d51d5111798f00b
Noam Chitayat <noam.chitayat+git@gmail.com>
2015-12-27 19:05:45 -0800

    Adding collision shapes to Town Tileset
    Taking advantage of Tiled's new Collision Editor to encode more collision data in the Tileset and remove some of the tedious work of setting up collisions for tiles that serve as obstacles.
    
    Since more collision information is encoded in the tileset itself, many of the collision rectangles explicitly placed in the Tiled maps’ collision layers were no longer necessary, and were therefore removed from the maps.

--- data/tilesets/town.tsx
+++ data/tilesets/town.tsx
@@ -8,24 +8,181 @@
   <terrain name="Ice" tile="-1"/>
   <terrain name="Water" tile="-1"/>
  </terraintypes>
- <tile id="0" terrain=",,,0"/>
- <tile id="1" terrain=",,0,0"/>
- <tile id="2" terrain=",,0,0"/>
- <tile id="3" terrain=",,0,0"/>
- <tile id="4" terrain=",,0,"/>
- <tile id="17" terrain=",0,,0"/>
- <tile id="18" terrain="0,0,0,0"/>
- <tile id="19" terrain="0,0,0,"/>
- <tile id="20" terrain="0,0,,0"/>
- <tile id="21" terrain="0,,0,"/>
- <tile id="34" terrain=",0,,0"/>
- <tile id="35" terrain="0,0,0,0"/>
- <tile id="36" terrain="0,,0,0"/>
- <tile id="37" terrain=",0,0,0"/>
- <tile id="38" terrain="0,,0,"/>
- <tile id="51" terrain=",0,,"/>
- <tile id="52" terrain="0,0,,"/>
- <tile id="55" terrain="0,,,"/>
+ <tile id="0" terrain=",,,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1" terrain=",,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2" terrain=",,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" terrain=",,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" terrain=",,0,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="-32" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="-32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="-32" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="-32" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="-32" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="-32" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="-32" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="17" terrain=",0,,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="18" terrain="0,0,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="19" terrain="0,0,0,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="20" terrain="0,0,,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="21" terrain="0,,0,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="34" terrain=",0,,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="35" terrain="0,0,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="36" terrain="0,,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="37" terrain=",0,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="38" terrain="0,,0,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="51" terrain=",0,,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="52" terrain="0,0,,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="55" terrain="0,,,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="71">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="72">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="75" terrain="1,1,1,1"/>
  <tile id="85" terrain="2,2,2,3"/>
  <tile id="86" terrain="2,2,3,3"/>
@@ -34,35 +191,120 @@
  <tile id="89" terrain="3,3,3,2"/>
  <tile id="90" terrain="3,3,2,3"/>
  <tile id="93" terrain="1,1,1,1"/>
+ <tile id="96">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="102" terrain="2,3,2,3"/>
  <tile id="103" terrain="3,3,3,3"/>
  <tile id="104" terrain="3,3,3,3"/>
  <tile id="105" terrain="3,2,3,2"/>
  <tile id="106" terrain="3,2,3,3"/>
  <tile id="107" terrain="2,3,3,3"/>
+ <tile id="109">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="111">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="119" terrain="2,3,2,3"/>
  <tile id="120" terrain="3,3,3,3"/>
  <tile id="121" terrain="3,3,3,3"/>
  <tile id="122" terrain="3,2,3,2"/>
+ <tile id="127">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="136" terrain="2,3,2,2"/>
  <tile id="137" terrain="3,3,2,2"/>
  <tile id="138" terrain="3,3,2,2"/>
  <tile id="139" terrain="3,2,2,2"/>
+ <tile id="140">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="143">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="145">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="153" terrain=",,,2"/>
  <tile id="154" terrain=",,2,2"/>
  <tile id="155" terrain=",,2,"/>
  <tile id="156" terrain="2,2,2,1"/>
  <tile id="157" terrain="2,2,1,2"/>
+ <tile id="161">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="163">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="164">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="165">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="168">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="169">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="170" terrain=",2,,2"/>
  <tile id="171" terrain="2,2,2,2"/>
  <tile id="172" terrain="2,,2,"/>
  <tile id="173" terrain="2,1,2,2"/>
  <tile id="174" terrain="1,2,2,2"/>
+ <tile id="177">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="179">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="187" terrain=",2,,"/>
  <tile id="188" terrain="2,2,,"/>
  <tile id="189" terrain="2,,,"/>
  <tile id="190" terrain="2,2,2,2"/>
  <tile id="191" terrain="2,2,2,2"/>
+ <tile id="195">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="204" terrain="1,1,1,2"/>
  <tile id="205" terrain="1,1,2,2"/>
  <tile id="206" terrain="1,1,2,1"/>
